c456b073a187e4e0c4b7bac6ff483d75f3873a26
Argir Argirov <dj.giro@gmail.com>
2017-10-03 16:06:26 -0400

    added redirects

--- server.rb
+++ server.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/portfolio' do
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby', 'Poker']
+  @interests = ['bitcoin', 'art', 'music', 'films', 'tea']
+  erb :about_me
+end
+
+get '/favourites' do
+  @links = ['http://www.amazon.ca', 'http://www.reddit.com', 'http://www.bittrex.com', 'http://www.pokerstars.com', 'http://www.ruby-doc.org']
+  erb :favourites
 end
